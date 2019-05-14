eb5dabbf273b50f6dbcf7fa3c93f9d78b2d73e22
Matthew Huber <unixmonky@gmail.com>
2019-05-14 15:38:38 -0400

    updated bluetooth to add flip option

--- bt.sh
+++ bt.sh
@@ -17,17 +17,14 @@ while [[ -n $1 ]]; do
   case $opt in
     a|a2dp )     SINK="a2dp_sink";;
     h|headset )  SINK="headset_head_unit";;
-    r|reset )    RESET=1; SINK=$(pactl info | grep "Default Sink" | awk -F. '{print $NF}');;
+    r|reset )    RESET=1; FLIP=1 SINK=$(pactl info | grep "Default Sink" | awk -F. '{print $NF}');;
+    f|flip )     FLIP=1; SINK=$(pactl info | grep "Default Sink" | awk -F. '{print $NF}');;
+    *)           usage;;
   esac
   shift
 done
 
 BLUEZCARD=$(pactl list cards short | egrep -o bluez.*[[:space:]])
-if [[ ${RESET} ]]; then
-  [[ ${SINk} == "a2dp_sink" ]] && ALTSINK="headset_head_unit" || ALTSUNK="a2dp_sink"
-  pactl set-card-profile $BLUEZCARD ${SINK}
-  sleep 1
-  pactl set-card-profile $BLUEZCARD ${ALTSINK}
-  sleep 1
-fi
-pactl set-card-profile $BLUEZCARD ${SINK}
+[[ ${SINK} == "a2dp_sink" ]] && ALTSINK="headset_head_unit" || ALTSINK="a2dp_sink"
+[[ ${FLIP} ]] && pactl set-card-profile $BLUEZCARD ${ALTSINK}
+[[ ${RESET} ]] && pactl set-card-profile $BLUEZCARD ${SINK}
